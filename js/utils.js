28d903511fb433ec5129ea97076512e5cef5b0bc
DuyVo96 <vominhduy.cokhi@gmail.com>
2022-07-27 14:34:58 +0700

    first push

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,39 @@
+function shuffle(arr) {
+  if (!Array.isArray(arr) || arr.length <= 2) return arr;
+  for (let i = arr.length - 1; i > 0; i--) {
+    // random a j number < i
+    const j = Math.floor(Math.random() * i);
+
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+}
+
 export const getRandomColorPairs = (count) => {
   // receive count --> return count * 2 random colors
   // using lib: https://github.com/davidmerfield/randomColor
-}
+
+  const colorList = [];
+  const hueList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome'];
+
+  // random "count" color
+
+  for (let i = 0; i < count; i++) {
+    // random color is provided by https://github.com/davidmerfield/randomColor
+    const color = window.randomColor({
+      luminosity: 'dark',
+      hue: hueList[i % hueList.length],
+    });
+    colorList.push(color);
+  }
+
+  // clone colorList to fullColorList and suffle it to each couple color have different position
+  const fullColorList = [...colorList, ...colorList];
+
+  // shuffle it
+
+  shuffle(fullColorList);
+
+  return fullColorList;
+};
